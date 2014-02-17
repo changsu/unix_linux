188fc595ac4639c514ca3e973288aeb0c140c207
chang <chang@fb.com>
2014-02-16 18:09:14 -0800

    add comment

--- ls_impl/ls_impl.c
+++ ls_impl/ls_impl.c
@@ -2,12 +2,14 @@
 #include <dirent.h>
 
 int main(int argc, char *argv[]) {
-  DIR *dp;
-  struct dirent *dirp;
+  DIR *dp; // directory pointer
+  struct dirent *dirp; // directory entry pointer
   if (argc != 2) {
     err_quit("usage: ls directory_name");
   } 
   
+  // opendir, readdir, closedir
+
   if ((dp = opendir(argv[1])) == NULL) {
     err_sys("can't open %s", argv[1]);
   }
