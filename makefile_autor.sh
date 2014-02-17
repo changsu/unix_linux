08eb1f15547971704231d7d67e00d2d09090aa46
chang <chang@fb.com>
2014-02-16 19:50:55 -0800

    change auto

--- makefile_autor.sh
+++ makefile_autor.sh
@@ -6,6 +6,7 @@ NEW_WORD="$1"
 UPPER_NEW_WORD=`echo $NEW_WORD | tr 'a-z' 'A-Z'`
 MAKEFILE_DIR="/Users/chang/Documents/Self_Learning/unix_linux/Makefile"
 FINAL_MAKEFILE="Makefile"
+TEMPLATE_FILE_PATH="/Users/chang/Documents/Self_Learning/unix_linux/test.c"
 
 if [ $# -ne 1 ]; then
     echo "Usage: ./makefile_autor.sh new_word"
@@ -16,6 +17,10 @@ if [ ! -f "Makefile" ] ; then
     cp MAKEFILE_DIR .
 fi
 
+# replace keyword in template makefile
 sed -e "s/$OLD_WORD/$NEW_WORD/g" -e "s/$UPPER_OLD_WORD/$UPPER_NEW_WORD/g" $MAKEFILE_DIR > $FINAL_MAKEFILE
+
+# copy over template .c file and rename
+cp $TEMPLATE_FILE_PATH "$1.c"
 
 
---
+++ standard_io/standard_io.c
@@ -0,0 +1,8 @@
+ #include <apue.h>
+ int main()
+ {
+         err_sys("some error found %d",getpid());
+
+         return 0 ;
+
+ }
