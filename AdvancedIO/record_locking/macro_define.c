a660eafd5b23deb245591c7d09a8fa7c1cbf11a4
chang <chang@fb.com>
2014-04-19 17:56:38 -0700

    asdf

---
+++ AdvancedIO/record_locking/macro_define.c
@@ -0,0 +1,10 @@
+#define read_lock(fd, offset, whence, len) \
+  lock_reg((fd), F_SETLK, F_RDLCK, (offset), (whence), (len))
+#define readw_lock(fd, offset, whence, len) \
+  lock_reg((fd), F_SETLKW, F_RDLCK, (offset), (whence), (len))
+#define write_lock(fd, offset, whence, len) \
+  lock_reg((fd), F_SETLK, F_WRLCK, (offset), (whence), (len))
+#define writew_lock(fd, offset, whence, len) \
+  lock_reg((fd), F_SETLKW, F_WRLCK, (offset), (whence), (len))
+#define un_lock(fd, offset, whence, len) \
+  lock_reg((fd), F_SETLK, F_UNLCK, (offset), (whence), (len))
